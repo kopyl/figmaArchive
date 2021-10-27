d26f93f6cb00090df19137440824d4aec053f0cb
kopyl <oleggggg1997@gmail.com>
2021-10-27 23:03:41 +0300

    Update figmaArchive.js

--- figmaArchive.js
+++ figmaArchive.js
@@ -1,8 +1,11 @@
-const getFigma = (timer) => {
-	alert("FUCK8")
-	try {
-		console.log(figma, timer)
-	} catch{console.log(222)}
-}
+// const getFigma = (timer) => {
+// 	alert("FUCK8")
+// 	try {
+// 		console.log(figma, timer)
+// 	} catch{console.log(222)}
+// }
 
-const timer = setInterval(() => {getFigma(timer)}, 200 )
+// const timer = setInterval(() => {getFigma(timer)}, 200 )
+
+
+alert('suck)
